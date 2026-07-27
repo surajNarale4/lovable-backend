9802ee0706eb0e6dba863334a045e5946e740e7e
surajNarale4 <rajnarale415@gmail.com>
2026-07-27 07:13:47 +0530

    feat: add Plan entity (relationships pending)

---
+++ lovable/src/main/java/io/suraj/projects/lovable/entity/Plan.java
@@ -0,0 +1,14 @@
+package io.suraj.projects.lovable.entity;
+
+public class Plan {
+
+    private Long id;
+    private long name;
+    private String StripePriceId;
+    private Integer maxProjects;
+    private Integer maxTokensPerDay;
+    private Integer maxPreviews;
+    private Boolean unlimnitedAi;
+
+    private Boolean active;
+}
