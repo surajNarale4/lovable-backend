49d2bd0cbd4d84a7f719b1703846ba3b9fa3cb69
surajNarale4 <rajnarale415@gmail.com>
2026-07-28 05:40:04 +0530

    feat: add ChatSession entity (relationships is pending )

---
+++ lovable/src/main/java/io/suraj/projects/lovable/entity/ChatSession.java
@@ -0,0 +1,13 @@
+package io.suraj.projects.lovable.entity;
+
+import java.time.Instant;
+
+public class ChatSession {
+
+    private Project project;
+    private User user;
+    private String title;
+    private Instant createAt;
+    private Instant updatedAt;
+    private Instant deletedAt;
+}
