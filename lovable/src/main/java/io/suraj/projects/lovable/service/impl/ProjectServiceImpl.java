efbbc11164a1ffe5a92d489c0c61ace3bb7eb665
surajNarale4 <rajnarale415@gmail.com>
2026-08-08 18:23:02 +0530

    feat: implement create project

--- lovable/src/main/java/io/suraj/projects/lovable/service/impl/ProjectServiceImpl.java
+++ lovable/src/main/java/io/suraj/projects/lovable/service/impl/ProjectServiceImpl.java
@@ -3,13 +3,24 @@ package io.suraj.projects.lovable.service.impl;
 import io.suraj.projects.lovable.dto.project.ProjectRequest;
 import io.suraj.projects.lovable.dto.project.ProjectResponse;
 import io.suraj.projects.lovable.dto.project.ProjectSummeryResponse;
+import io.suraj.projects.lovable.entity.Project;
+import io.suraj.projects.lovable.entity.User;
+import io.suraj.projects.lovable.mapper.ProjectMapper;
+import io.suraj.projects.lovable.repository.ProjectRepository;
+import io.suraj.projects.lovable.repository.UserRepository;
 import io.suraj.projects.lovable.service.ProjectService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProjectServiceImpl implements ProjectService {
+
+    private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
+    private final ProjectMapper projectMapper;
     @Override
     public List<ProjectSummeryResponse> getUserProjects(Long userId) {
         return List.of();
@@ -21,8 +32,15 @@ public class ProjectServiceImpl implements ProjectService {
     }
 
     @Override
-    public ProjectResponse createProject(ProjectRequest request) {
-        return null;
+    public ProjectResponse createProject(ProjectRequest request, Long userId) {
+        User user= userRepository.findById(userId).orElseThrow();
+        Project project=Project.builder()
+                .owner(user)
+                .name(request.name())
+                .build();
+        return projectMapper.toProjectResponse(projectRepository.save(project));
+
+
     }
 
     @Override
