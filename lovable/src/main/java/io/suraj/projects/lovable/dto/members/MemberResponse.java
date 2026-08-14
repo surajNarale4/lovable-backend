73ebd248ec4d1c644610bb46a1a01710e01e8645
surajNarale4 <rajnarale415@gmail.com>
2026-08-14 22:36:08 +0530

    feat: member mapper added for ProjectMember->MemberResponse

--- lovable/src/main/java/io/suraj/projects/lovable/dto/members/MemberResponse.java
+++ lovable/src/main/java/io/suraj/projects/lovable/dto/members/MemberResponse.java
@@ -1,9 +1,11 @@
 package io.suraj.projects.lovable.dto.members;
 
 import io.suraj.projects.lovable.entity.enums.ProjectRole;
+import lombok.Builder;
 
 import java.time.Instant;
 
+@Builder
 public record MemberResponse(
         Long id,
         String email,
--- lovable/src/main/java/io/suraj/projects/lovable/service/impl/ProjectMemberServiceImpl.java
+++ lovable/src/main/java/io/suraj/projects/lovable/service/impl/ProjectMemberServiceImpl.java
@@ -3,20 +3,28 @@ package io.suraj.projects.lovable.service.impl;
 import io.suraj.projects.lovable.dto.members.InviteMemberRequest;
 import io.suraj.projects.lovable.dto.members.MemberResponse;
 import io.suraj.projects.lovable.entity.ProjectMember;
+import io.suraj.projects.lovable.repository.ProjectMemberRepository;
+import io.suraj.projects.lovable.repository.UserRepository;
 import io.suraj.projects.lovable.service.ProjectMemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProjectMemberServiceImpl implements ProjectMemberService {
+
+    private final ProjectMemberRepository projectMemberRepository;
+
     @Override
     public List<ProjectMember> getAllProjectMembers(Long userId, Long projectId) {
-        return List.of();
+        return projectMemberRepository.findAll();
     }
 
     @Override
     public MemberResponse inviteMember(Long userId, String projectId, InviteMemberRequest request) {
+
         return null;
     }
 
