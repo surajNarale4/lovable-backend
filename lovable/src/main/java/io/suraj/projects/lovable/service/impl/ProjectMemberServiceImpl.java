29b4be23c086fafd64768e92f9de1c023df209a7
surajNarale4 <rajnarale415@gmail.com>
2026-08-15 12:02:41 +0530

    feat: add and fix project member management

--- lovable/src/main/java/io/suraj/projects/lovable/service/impl/ProjectMemberServiceImpl.java
+++ lovable/src/main/java/io/suraj/projects/lovable/service/impl/ProjectMemberServiceImpl.java
@@ -4,12 +4,14 @@ import io.suraj.projects.lovable.dto.members.InviteMemberRequest;
 import io.suraj.projects.lovable.dto.members.MemberResponse;
 import io.suraj.projects.lovable.entity.Project;
 import io.suraj.projects.lovable.entity.ProjectMember;
+import io.suraj.projects.lovable.entity.ProjectMemberId;
 import io.suraj.projects.lovable.mapper.MemberMapper;
 import io.suraj.projects.lovable.mapper.ProjectMapper;
 import io.suraj.projects.lovable.repository.ProjectMemberRepository;
 import io.suraj.projects.lovable.repository.ProjectRepository;
 import io.suraj.projects.lovable.repository.UserRepository;
 import io.suraj.projects.lovable.service.ProjectMemberService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import io.suraj.projects.lovable.entity.User;
@@ -21,6 +23,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ProjectMemberServiceImpl implements ProjectMemberService {
 
     private final ProjectMemberRepository projectMemberRepository;
@@ -32,7 +35,7 @@ public class ProjectMemberServiceImpl implements ProjectMemberService {
     public List<MemberResponse> getAllProjectMembers(Long userId, Long projectId) {
         Project project=projectRepository.findByAccessbileProjects(userId,projectId).orElseThrow();
         List<MemberResponse> projectMembers = new ArrayList<>();
-        projectMembers.add(memberMapper.toMamberMapper(project.getOwner()));
+        projectMembers.add(memberMapper.toMemberMapper(project.getOwner()));
         projectMemberRepository.findByProjectId(projectId).stream()
                 .map(members-> projectMembers.add(memberMapper.toMemberMapper(members)))
                 .toList();
@@ -43,32 +46,72 @@ public class ProjectMemberServiceImpl implements ProjectMemberService {
 
 
     public MemberResponse inviteMember(Long userId, Long projectId, InviteMemberRequest request) {
-        User user=userRepository.findById(userId).orElseThrow();
-        Project project = projectRepository.findById(projectId).orElseThrow();
+        Project project = projectRepository.findByAccessbileProjects(userId,projectId).orElseThrow();
+        /*
+         * Users cannot send invitations on behalf of someone else.
+         */
+        if(!project.getOwner().getId().equals(userId)){
+            throw new RuntimeException("This action is not allowed");
+        }
+
+        User invitee = userRepository.findByEmail(request.email()).orElseThrow();
+        if(invitee.getId().equals(userId)){
+            throw new RuntimeException("cannot invite your self");
+        }
+        ProjectMemberId projectMemberId = new ProjectMemberId(projectId, invitee.getId());
+
         ProjectMember projectMember = ProjectMember.builder()
+                .id(projectMemberId)
                 .project(project)
-                .user(user)
+                .user(invitee)
                 .role(request.role())
                 .invitedAt(Instant.now())
                 .build();
         projectMemberRepository.save(projectMember);
+        return memberMapper.toMemberMapper(projectMember);
 
-        return MemberResponse.builder()
-                .invitedAt(projectMember.getInvitedAt())
-                .avtarUrl(user.getAvtarUrl())
-                .name(user.getName())
-                .email(user.getEmail())
-                .id(user.getId())
-                .build();
     }
 
     @Override
     public MemberResponse updateMemberRole(Long projectId, Long memberId, InviteMemberRequest request, Long userId) {
-        return null;
+
+        Project project = projectRepository.findByAccessbileProjects(userId,projectId).orElseThrow();
+        /*
+         * Users cannot send invitations on behalf of someone else.
+         */
+        if(!project.getOwner().getId().equals(userId)){
+            throw new RuntimeException("This action is not allowed");
+        }
+        ProjectMember projectMember = projectMemberRepository.findById(
+                ProjectMemberId.builder()
+                        .userId(memberId)
+                        .projectId(projectId)
+                        .build()
+        ).orElseThrow();
+        projectMember.setRole(request.role());
+        projectMemberRepository.save(projectMember); //saving explicitly as well
+       return memberMapper.toMemberMapper(projectMember);
     }
 
     @Override
     public MemberResponse deleteProjectMember(Long projectId, Long memberId, Long userId) {
+        Project project = projectRepository.findByAccessbileProjects(userId,projectId).orElseThrow();
+        /*
+         * Users cannot send invitations on behalf of someone else.
+         */
+        if(!project.getOwner().getId().equals(userId)){
+            throw new RuntimeException("This action is not allowed");
+        }
+        if(project.getOwner().getId().equals(memberId)){
+            throw new RuntimeException("you can't delete yourself using this api");
+        }
+        ProjectMemberId projectMemberId=ProjectMemberId.builder()
+                .projectId(projectId)
+                .userId(memberId)
+                .build();
+        if(!projectMemberRepository.existsById(projectMemberId))
+            throw new RuntimeException("Project is not exists for given member");
+        projectMemberRepository.deleteById(projectMemberId);
         return null;
     }
 }
--- lovable/src/test/java/io/suraj/projects/lovable/LovableApplicationTests.java
+++ lovable/src/test/java/io/suraj/projects/lovable/LovableApplicationTests.java
@@ -1,13 +1,40 @@
 package io.suraj.projects.lovable;
 
+import io.suraj.projects.lovable.dto.project.ProjectResponse;
+import io.suraj.projects.lovable.entity.Project;
+import io.suraj.projects.lovable.entity.User;
+import io.suraj.projects.lovable.mapper.ProjectMapper;
+import io.suraj.projects.lovable.service.ProjectService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LovableApplicationTests {
 
+	@Autowired
+	private ProjectMapper projectMapper;
+	@Autowired
+	private ProjectService projectService;
+
+
 	@Test
 	void contextLoads() {
+//		User user = User.builder()
+//				.name("suraj")
+//				.email("suraj@123")
+//				.avtarUrl("/av/cool")
+//				.build();
+//		Project project = Project.builder()
+//				.name("lovable")
+//				.owner(user)
+//				.build();
+//		ProjectResponse projectResponse=projectMapper.toProjectResponse(project);
+//		System.out.println(projectResponse);
+
+//		System.out.println(projectService.getUserProjects(1L));
+
+
 	}
 
 }
