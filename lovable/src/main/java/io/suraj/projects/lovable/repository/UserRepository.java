3dc324ccb639f24d11446c40d8bbf11dafad825e
surajNarale4 <rajnarale415@gmail.com>
2026-08-15 11:54:26 +0530

    feat: added findByEmail method in UserRepository

--- lovable/src/main/java/io/suraj/projects/lovable/repository/UserRepository.java
+++ lovable/src/main/java/io/suraj/projects/lovable/repository/UserRepository.java
@@ -3,7 +3,9 @@ package io.suraj.projects.lovable.repository;
 import io.suraj.projects.lovable.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
 }
