d6341a6b5645167e06b45207453fb791cce91cf4
surajNarale4 <rajnarale415@gmail.com>
2026-08-15 11:53:21 +0530

    feat: added missing endpoints in auth controller

--- lovable/src/main/java/io/suraj/projects/lovable/controller/AuthController.java
+++ lovable/src/main/java/io/suraj/projects/lovable/controller/AuthController.java
@@ -8,6 +8,8 @@ import io.suraj.projects.lovable.dto.auth.UserProfileRespose;
 import io.suraj.projects.lovable.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,16 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private  AuthService authService; //later will make final once implements
-
+    private final AuthService authService; //later will make final once implements
+    @PostMapping("/signup")
     public ResponseEntity<AuthResponse> signup(SignupRequest request){
         return ResponseEntity.ok(authService.signup(request));
     }
 
+    @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(LoginRequest request){
         return ResponseEntity.ok(authService.login(request));
     }
 
+    @GetMapping("/me")
     public ResponseEntity<UserProfileRespose> getProfile(){
         Long userId =1L;
         return ResponseEntity.ok(authService.getProfile(userId));
