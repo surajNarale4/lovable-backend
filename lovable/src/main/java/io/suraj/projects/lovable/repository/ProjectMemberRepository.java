e29286b48e06d07be10081d1063221f8fabb642f
surajNarale4 <rajnarale415@gmail.com>
2026-08-20 11:28:14 +0530

    feat: added findByProjectIdAndUserIdAndRole for getting ProjectMember OWNER

--- lovable/src/main/java/io/suraj/projects/lovable/repository/ProjectMemberRepository.java
+++ lovable/src/main/java/io/suraj/projects/lovable/repository/ProjectMemberRepository.java
@@ -2,6 +2,8 @@ package io.suraj.projects.lovable.repository;
 
 import io.suraj.projects.lovable.entity.ProjectMember;
 import io.suraj.projects.lovable.entity.ProjectMemberId;
+import io.suraj.projects.lovable.entity.enums.ProjectRole;
+import lombok.Builder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +14,5 @@ import java.util.Optional;
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, ProjectMemberId> {
     List<ProjectMember> findByProjectId(Long projectId);
     Optional<ProjectMember> findById(ProjectMemberId projectMemberId);
-
+    Optional<ProjectMember> findByProjectIdAndUserIdAndRole(Long projectId , Long userId, ProjectRole role);
 }
