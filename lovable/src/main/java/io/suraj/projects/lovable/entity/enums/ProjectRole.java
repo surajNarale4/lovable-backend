47128d972e6289cbdc43d9b85ab8d340eec284f6
surajNarale4 <rajnarale415@gmail.com>
2026-08-29 08:04:46 +0530

    feat: keycloak role realm added USER as default for all user at signup

--- lovable/src/main/java/io/suraj/projects/lovable/entity/enums/ProjectRole.java
+++ lovable/src/main/java/io/suraj/projects/lovable/entity/enums/ProjectRole.java
@@ -1,5 +1,9 @@
 package io.suraj.projects.lovable.entity.enums;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+
 public enum ProjectRole {
     EDITOR,VIEWER,OWNER
 }
